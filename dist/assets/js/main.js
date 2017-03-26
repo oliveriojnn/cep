73734bbcf4e2b1941a9171ca5e32459516d0e0be
Olivério Neto <oliveriojnn@gmail.com>
2017-03-26 19:21:00 -0300

    Adicionado uma lista de CEPs já pesquisados

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,27 +3,33 @@ var app = new Vue({
 	data: {
 		message: 'Busque o endereço do seu CEP!',
 		cep:'',
-		status:false,
-		resultado:{}
+		erro:false,
+		resultado:{},
+		ceps:[]
 	},
 	methods:{
 		send: function(e) {
 			e.preventDefault();
 			var self = this;
-			self.$http.get('https://viacep.com.br/ws/' + this.cep + '/json/').then(function(result) {
-				self.resultado = result.data;
-				this.status = false;
-				self.cep = '';
+			self.$http.get('https://viacep.com.br/ws/' + this.cep + '/json/')
+			.then(function(result) {
+				this.resultado = result.data;
+				this.erro = false;
+				this.cep = '';
 				$('#cep').focus();
+				if (this.resultado.logradouro) {
+					this.ceps.push(self.resultado);
+				}
 			})
 			.catch(function(err) {
-				this.status = true;
+				this.erro = true;
 				this.resultado = {};
 				this.cep = '';
 				$('#cep').focus();
 			})
 		},
 		limpar: function(e) {
+			this.ceps = [];
 			e.preventDefault();
 			this.status = false;
 			this.cep = '';
